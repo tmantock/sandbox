f834710e9fa2b27d6ef91eeba312f9998b6f0159
Tevin Mantock <tmantock@gmail.com>
2016-04-18 17:44:19 -0700

    Memory Match v1 - Tevin Mantock

---
+++ JS Practice.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Mantock on 3/29/16.
+ */
+/*startup reading
+ INTRO: http://www.w3schools.com/js/js_intro.asp
+ Variables: http://www.w3schools.com/js/js_variables.asp
+
+ /*Make 5 variables with one each of the following javascript data types:
+ string
+ number
+ boolean
+ null
+ undefined
+ */
+
+var string_var = "Hello";
+var number_var = 33;
+var boolean_var = true;
+var null_var = null;
+var undefined_var = "undefined";
+
+
+
+
+/*Make an array with multiple strings in it.  In the strings put skills you want to master in this course*/
+
+var desired_skills = ["Web Development","JavaScript","Coding Knowledge","Problem Solving"];
+
+
+
+
+
+
+
+/*Make an object with 3 key:value pairs.  They keys will be
+ name
+ hobby
+ first job (with the space)
+ */
+
+var biography_object = {
+    name: "Tevin Mantock",
+    hobby: "Video Games",
+    first_job: "Bus drvier"
+};
